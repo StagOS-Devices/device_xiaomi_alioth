dba721865ed7e6c232720fd6b2fdb1fc4ecf8d31
Reaper3110 <bhuvneshwar.ghate.ai@ghrietn.raisoni.net>
2022-11-30 23:03:59 +0530

    alioth: Stagify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_alioth.mk
+    $(LOCAL_DIR)/stag_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_alioth-user \
-    lineage_alioth-userdebug \
-    lineage_alioth-eng
+    stag_alioth-user \
+    stag_alioth-userdebug \
+    stag_alioth-eng
